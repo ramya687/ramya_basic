bc52ff578cd62cd9000b0b7c923ae4ae6a6b1b53
ramya687 <pramyadevipramyadevi@gmail.com>
2025-05-12 15:45:19 +0530

    Add files via upload

---
+++ c.vetias/area.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int a=5,b=10;
+    printf("\n area of rectangle %d",a+b);
+    int d=2*(a+b);
+    printf("\n perimeter of a rectangle %d",d);
+    return 0;
+}
---
+++ c.vetias/faren.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+    int a=50;
+    int c=(a*9/5)+32;
+    printf("%d",c);
+}
