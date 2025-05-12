ef7c995cee24fc9437dfcf453cf0711659fadbc0
ramya687 <pramyadevipramyadevi@gmail.com>
2025-05-12 15:28:14 +0530

    Add files via upload

---
+++ c.vetias/day1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    printf("ramya:2005 \n");
+    printf("erode:tn \n");
+    int a;
+    a=3;
+    printf("%d \n",a);
+    float b;
+    b=3.8;
+    printf("%f \n",b);
+    char c;
+    c='D';
+    printf("%c \n",c);
+    return 0;
+    
+}
---
+++ c.vetias/swap.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int a=9,b=7,temp;
+    temp=a;
+    a=b;
+    b=temp;
+    printf("a=%d \n b=%d \n",a,b);
+
+}
