2532dd0805d103216d71007de6e7ca6b464d4ceb
ramya687 <pramyadevipramyadevi@gmail.com>
2025-05-16 11:02:18 +0530

    Add files via upload

---
+++ amstrong.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    while(a=a+1){
+    printf("%d",a);
+}
+    if(a>0)
+    printf("amstrong num");
+    else
+    printf("not");
+    return 0;
+}
